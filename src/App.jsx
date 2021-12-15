115d2297a2bec354bfa84978b649ca297bc0cd93
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-12-15 17:49:23 -0600

    changed signin__header variable name

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './css/style.css';
-import './components/navbar/Navbar';
-import Homepage from './components/homepage/Homepage';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import Navbar from './components/navbar/Navbar';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./css/style.css";
+import "./components/navbar/Navbar";
+import Homepage from "./components/homepage/Homepage";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Navbar from "./components/navbar/Navbar";
 
-import './sass/main.scss';
+import "./sass/main.scss";
 
 export default function App() {
-  const [sessionToken, setSessionToken] = useState('');
+  const [sessionToken, setSessionToken] = useState("");
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      setSessionToken(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
     }
   }, []);
 
   const updateToken = (newToken) => {
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
     setSessionToken(newToken);
   };
 
   const clearToken = () => {
     localStorage.clear();
-    setSessionToken('');
+    setSessionToken("");
   };
 
   // const protectedViews = () => {
@@ -43,20 +43,20 @@ export default function App() {
   return (
     <div>
       <header>
-        <h1 className="Signin__header">Welcome to Horror House</h1>
+        <h1 className='signin__header'>Welcome to Horror House</h1>
       </header>
       <main>
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Login token={sessionToken} updateToken={updateToken} />
           </Route>
-          <Route exact path="/signup">
+          <Route exact path='/signup'>
             <Signup token={sessionToken} updateToken={updateToken} />
           </Route>
 
-          {sessionToken === localStorage.getItem('token') && (
+          {sessionToken === localStorage.getItem("token") && (
             <>
-              <Route exact path="/home">
+              <Route exact path='/home'>
                 <Homepage token={sessionToken} />
               </Route>
               <header>
