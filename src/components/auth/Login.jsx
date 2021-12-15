24502997b6950656d0ea2f1f979d5a16a0db9c27
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-12-15 17:15:35 -0600

    commit

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Login({ updateToken }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUsername = (e) => setUsername(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
@@ -15,11 +15,11 @@ export default function Login({ updateToken }) {
 
     try {
       const fetchResults = await fetch(`http://localhost:3001/user/`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({ username, password }),
         headers: new Headers({
-          'Content-Type': 'application/json'
-        })
+          "Content-Type": "application/json",
+        }),
       });
       const json = await fetchResults.json();
       if (!json.user || !json.sessionToken) {
@@ -27,43 +27,43 @@ export default function Login({ updateToken }) {
         return;
       }
       updateToken(json.sessionToken);
-      history.push('/home');
+      history.push("/home");
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <div className="signin">
-      <form className="signin__form" onSubmit={handleSubmit}>
-        <h3 className="signin__h3">Signin</h3>
-        <div className="signin__container">
+    <div className='signin'>
+      <form className='signin__form' onSubmit={handleSubmit}>
+        <h3 className='signin__h3'>Signin</h3>
+        <div className='signin__container'>
           <input
-            type="username"
+            type='username'
             onChange={handleUsername}
-            name="username"
-            placeholder="Username"
+            name='username'
+            placeholder='Username'
             required
             value={username}
-            className="signin__input"
+            className='signin__input'
           />
           <input
-            type="password"
+            type='password'
             onChange={handlePassword}
-            name="password"
-            placeholder="Password"
-            type="password"
+            name='password'
+            placeholder='Password'
+            type='password'
             required
             value={password}
-            className="signin__input"
+            className='signin__input'
           />
-          <button className="signin__button" type="submit">
+          <button className='signin__button' type='submit'>
             Login
           </button>
         </div>
-      </form>{' '}
-      <p className="signin__toggle" onClick={() => history.push('./signup')}>
-        Don't have an account? Sign up here.{' '}
+      </form>{" "}
+      <p className='signin__toggle' onClick={() => history.push("./signup")}>
+        Don't have an account? Sign up here.{" "}
       </p>
     </div>
   );
