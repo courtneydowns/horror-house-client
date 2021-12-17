838e0a485d5a9ef2db93ccbcb502ea935a16532e
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-12-16 21:21:29 -0600

    styling login page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -10,13 +10,12 @@ import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import { FaGhost } from "react-icons/fa";
 import house from "../../assets/house.jpg";
+import { FaGhost } from "react-icons/fa";
+import Typography from "@mui/material/Typography";
 import "../../sass/main.scss";
 
-export default function Login({ updateToken }) {
+export default function Login({ updateToken, toggleView }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -49,107 +48,106 @@ export default function Login({ updateToken }) {
   };
 
   return (
-    <Grid container component='main' sx={{ height: "100vh" }}>
-      <CssBaseline />
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={7}
-        sx={{
-          backgroundImage: `url(${house})`,
-          backgroundRepeat: "no-repeat",
-          backgroundColor: (t) =>
-            t.palette.mode === "light"
-              ? t.palette.grey[50]
-              : t.palette.grey[900],
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {" "}
-        <header>
-          <h1 className='signin__header'>Welcome back to Horror House!</h1>
-        </header>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={5}
-        component={Paper}
-        elevation={6}
-        square
-      ></Grid>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "#cc0000" }}>
-          <FaGhost style={{ fontSize: "3rem" }} />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Sign in
-        </Typography>
-        <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            id='username'
-            label='Username'
-            name='username'
-            autoComplete='username'
-            autoFocus
-            validate
-            onChange={handleUsername}
-          />
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            validate
-            autoComplete='current-password'
-            onChange={handlePassword}
-          />
-          {/* <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            /> */}
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 2 }}
+    <div>
+      <header>
+        <h1 className='signin__header'>Welcome Back to Horror House</h1>
+      </header>
+      <Grid container component='main' sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${house})`,
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            Sign In
-          </Button>
-          <Grid container>
-            {/* <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Forgot password?
-                </Link>
-              </Grid> */}
-            {/* <Grid item> */}
-            <p
-              style={{ fontSize: "1.5rem", cursor: "pointer" }}
-              className='signup__toggle'
-              onClick={() => history.push("./signup")}
+            <Avatar sx={{ m: 1, bgcolor: "#cc0000" }}>
+              <FaGhost style={{ fontSize: "3rem" }} />
+            </Avatar>
+            <Typography component='h1' variant='h5'>
+              Sign in
+            </Typography>
+            <Box
+              component='form'
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
             >
-              Already have an account? Sign up here.{" "}
-            </p>
-            {/* </Grid> */}
-          </Grid>
-        </Box>
-      </Box>
-    </Grid>
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='username'
+                label='Username'
+                name='username'
+                autoComplete='username'
+                value={username}
+                onChange={handleUsername}
+                autoFocus
+              />
+              <TextField
+                margin='normal'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                value={password}
+                onChange={handlePassword}
+                autoComplete='current-password'
+              />
+              {/* <FormControlLabel
+                control={<Checkbox value='remember' color='primary' />}
+                label='Remember me'
+              /> */}
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                {/* <Grid item xs>
+                  <Link href='#' variant='body2'>
+                    Forgot password?
+                  </Link>
+                </Grid> */}
+                <Grid item>
+                  <p
+                    style={{ fontSize: "1.5rem", cursor: "pointer" }}
+                    className='signup__toggle'
+                    onClick={() => history.push("./")}
+                  >
+                    Already have an account? Sign up here.{" "}
+                  </p>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
