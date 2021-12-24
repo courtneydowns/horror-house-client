d769cb830a9c8e12f34b887534d06255c21eb6b8
Courtney Downs <courtneydowns@Courtneys-MacBook-Pro.local>
2021-12-24 11:15:10 -0600

    commit

--- src/components/SearchMovie/SearchResult.jsx
+++ src/components/SearchMovie/SearchResult.jsx
@@ -1,59 +1,57 @@
-// import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
-// let debounceTimeout;
+let debounceTimeout;
 
-// export default function SearchResult() {
-//   const searchInput = useRef(null);
-//   const [searchResult, setSearchResult] = useState([]);
+export default function SearchResult() {
+  const searchInput = useRef(null);
+  const [searchResults, setSearchResults] = useState([]);
+  const [query, setQuery] = useState("");
 
-//   searchInput.current?.focus();
+  searchInput.current?.focus();
 
-//   async function searchMovies(searchQuery) {
-//     const resp = await fetch(
-//       `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=f6c2597101b3b0f8d6fcff87b23eaea3&&with_genres=27`
-//     );
-//     const json = await resp.json();
-//     const { results } = json;
+  async function searchMovies(searchQuery) {
+    const resp = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=f6c2597101b3b0f8d6fcff87b23eaea3&&with_genres=27`
+    );
+    const json = await resp.json();
+    const { results } = json;
 
-//     setSearchResult(results || []);
-//   }
+    setSearchResults(results || []);
+  }
 
-//   return (
-//     <div className="search-result">
-//       <h2 className="search-result__header">Search for Movie by Title</h2>
+  
 
-//       <input
-//         ref={searchInput}
-//         placeholder="Movie title"
-//         onInput={(ev) => {
-//           clearTimeout(debounceTimeout);
+  return (
+    <div className="search-result">
+      <h2 className="search-result__header">Search for Movie by Title</h2>
 
-//           debounceTimeout = setTimeout(() => {
-//             searchMovies(ev.target.value);
-//           }, 250);
-//         }}
-//       />
+      <input
+        ref={searchInput}
+        onChange={ev => setQuery(ev.target.value)}
+        placeholder="Movie title"
+        // onInput={(ev) => {
+        //   clearTimeout(debounceTimeout);
 
-// <button onClick={searchMovies}>Search</button>
-// {searchResult && (
-//         <div>
-//           <h2>Title</h2>
-//           <h2>{searchResult.title}</h2>
-//           <img
-//         src={`https://image.tmdb.org/t/p/original${searchResult.poster_path}`}
-//         alt="Movie poster"
-//       />
-//           <h4>IMDB Rating</h4>
-//           <h4>{searchResult.vote_average}</h4>
-//           <h4>IMDb Rating</h4>
-//           <h4>{searchResult.imdbRating}</h4>
-//           <h4>Release Date</h4>
-//           <h4>{searchResult.release_date}</h4>
-//           <h4>Overview</h4>
-//           <h6>{searchResult.overview}</h6>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+        //   debounceTimeout = setTimeout(() => {
+        //     searchMovies(ev.target.value);
+        //   }, 250);
+        // }}
+      />
+
+<button onClick={() => searchMovies(query)}>Search</button>
+{searchResults.map(searchResult => (
+        <div key={searchResult}>
+          <h1>Title: {searchResult.title}</h1>
+          <img
+        src={`https://image.tmdb.org/t/p/original${searchResult.poster_path}`}
+        alt="Movie poster"
+      />
+          <h3>IMDB Rating: {searchResult.vote_average}</h3>
+          <h3>Release Date: {searchResult.release_date}</h3>
+          <h4>Overview: {searchResult.overview}</h4>
+        </div>
+    ))}
+    </div>
+  );
+};
     
